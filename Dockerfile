04a9ad0a9f546f9becdbce327f7b54ebe6d550bd
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2023-05-12 23:18:42 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:jammy-20230425
+
+RUN apt update \
+ && apt install python3 -y \
+ && apt install uvicorn -y \
+ && apt install python3-pip -y
+
+RUN pip3 install fastapi==0.95.1 \
+&& pip3 install pandas==2.0.1 \
+&& pip3 install tabulate==0.9.0 \
+&& mkdir -p /app/data
+
+ADD main.py /app
+
+EXPOSE 8000
+
+CMD cd /app && uvicorn main:app --host 0.0.0.0 --port 8000
---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from tabulate import tabulate
+from fastapi.responses import HTMLResponse, PlainTextResponse
+import pandas as pd
+from pydantic import BaseSettings
+
+import os
+import datetime
+
+
+class Settings(BaseSettings):
+    openapi_url: str = ""
+
+
+settings = Settings()
+app = FastAPI(openapi_url=settings.openapi_url)
+
+user_data = {
+        "username": "inserter",
+        "password": "inserterlikeinfactoriogamebtwirecommendit",
+}
+
+class Input(BaseModel):
+    username: str
+    password: str
+    raspberry_temp: float
+    room_temp: float
+    humidity: int
+    timestamp: int
+
+
+@app.post("/input/")
+async def insert_data(inp: Input):
+    if inp.username == user_data["username"] and inp.password == user_data["password"]:
+        df = pd.DataFrame({"timestamp": datetime.datetime.fromtimestamp(inp.timestamp),
+                        "raspberry_temp": inp.raspberry_temp,
+                        "room_temp": inp.room_temp,
+                        "humidity": inp.humidity}, index=[0])
+        if os.path.isfile("data/data.csv"):
+            df.to_csv("data/data.csv", header=False, mode="a", index=False)
+        else:
+            df.to_csv("data/data.csv", index=False)
+        return {"status": "ok"}
+    else:
+        return {"status": "error"}
+
+@app.get("/get_data/", response_class=HTMLResponse)
+async def get_data():
+    try:
+        df = pd.read_csv("data/data.csv")
+        df.set_index("timestamp", inplace=True)
+    except FileNotFoundError:
+        return "no data"
+    return tabulate(df, headers='keys', tablefmt='html')
+
+@app.get("/get_data_raw/", response_class=PlainTextResponse)
+async def get_data_raw():
+    try:
+        df = pd.read_csv("data/data.csv")
+        df.set_index("timestamp", inplace=True)
+    except FileNotFoundError:
+        return "no data"
+    return tabulate(df, headers='keys', tablefmt='pretty')
