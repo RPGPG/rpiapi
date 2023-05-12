f2402d1ef09deb15e1908008943afe8de378b6a1
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2023-05-13 01:31:22 +0200

    add --restart=unless-stopped

--- recreate.sh
+++ recreate.sh
@@ -3,4 +3,4 @@
 mkdir -p ./data
 docker build -t rpi:rpi . &&
 docker rm --force rpiapi
-docker run -d -p [::]:8000:8000 --name rpiapi -v /root/rpiapi/data:/app/data rpi:rpi
+docker run -d -p [::]:8000:8000 --name rpiapi -v /root/rpiapi/data:/app/data --restart=unless-stopped rpi:rpi
