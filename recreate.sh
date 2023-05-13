c98eb6c4d39ba2e682ff0c2a45f71d08332c02d5
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2023-05-13 02:00:49 +0200

    add cd to target path

--- recreate.sh
+++ recreate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p ./data
-docker build -t rpi:rpi /home/gh-actions/rpiapi &&
+cd /home/gh-actions/rpiapi && docker build -t rpi:rpi . &&
 docker rm --force rpiapi
 docker run -d -p [::]:8000:8000 --name rpiapi -v /root/rpiapi/data:/app/data --restart=unless-stopped rpi:rpi
