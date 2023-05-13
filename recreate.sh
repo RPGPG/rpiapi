bf182e5d69c5092cae5c0d3b5c10c7c39442dfc9
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2023-05-13 02:07:41 +0200

    change data path

--- recreate.sh
+++ recreate.sh
@@ -3,4 +3,4 @@
 mkdir -p ./data
 cd /home/gh-actions/rpiapi && docker build -t rpi:rpi . &&
 docker rm --force rpiapi
-docker run -d -p [::]:8000:8000 --name rpiapi -v /root/rpiapi/data:/app/data --restart=unless-stopped rpi:rpi
+docker run -d -p [::]:8000:8000 --name rpiapi -v /gh-actions/rpiapi/data:/app/data --restart=unless-stopped rpi:rpi
